4d50a856ba85fa6a0300694a23c2772dba6caacb
Kirill Anisimov <anisimov_k@icloud.com>
2020-02-16 19:01:09 +0300

    Initial Commit

---
+++ QuizGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizGame
+//
+//  Created by Кирилл Анисимов on 16.02.2020.
+//  Copyright © 2020 Кирилл Анисимов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
